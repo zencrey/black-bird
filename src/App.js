d95c3b660aa8fb65a0eb12380f486c1f090480a5
zencrey <56424267+zencrey@users.noreply.github.com>
2023-07-20 17:34:25 +0530

    final edit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          link to the propelleraero
+          link to propelleraero
         </a>
       </header>
     </div>
